9ef52891427ee9b01fa66546c5f211ba659e7524
Thor Malmby Jørgin <tjoer21@student.sdu.dk>
2023-09-22 14:49:10 +0200

    Starwars flatlist and native correct in package.json

--- components/starWars.tsx
+++ components/starWars.tsx
@@ -1,22 +1,27 @@
 import {useState} from "react";
-import {View, Text} from "react-native";
+import {View, Text, SafeAreaView, FlatList} from "react-native";
 
 export const StarWars = () => {
     let [data, setData] = useState({})
-    fetch("https://swapi.dev/api/people/1")
-        .then(response=> response.json())
-        .then(myData=>{
+    fetch("https://swapi.dev/api/people")
+        .then(response => response.json())
+        .then(myData => {
             setData(myData)
         })
 
 
     return (
         <View>
-            <Text>{data["name"]}</Text>
-            <Text>{data["height"]}</Text>
-            <Text>{data["mass"]}</Text>
-            <Text>{data["eye_color"]}</Text>
-            <Text>{data["hair_color"]}</Text>
+            <FlatList data={data["results"]} renderItem={({item}) =>
+                <Text>{item["name"]}</Text>
+            }
+            />
+            {/*<Text>{data["name"]}</Text>*/}
+            {/*<Text>{data["height"]}</Text>*/}
+            {/*<Text>{data["mass"]}</Text>*/}
+            {/*<Text>{data["eye_color"]}</Text>*/}
+            {/*<Text>{data["hair_color"]}</Text>*/}
+            {/*<Text>{JSON.stringify(data["results"])}</Text>*/}
         </View>
     );
 }
