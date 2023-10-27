bdac62ee581c8fde485656440323a95080737918
TobiasBak <tobib21@student.sdu.dk>
2023-10-27 10:42:07 +0200

    Linter fixes

--- App.tsx
+++ App.tsx
@@ -11,12 +11,12 @@ import {NavigatorParamList} from "./screens/ScreenParams";
 const DiscoveryStack = createNativeStackNavigator<NavigatorParamList>();
 
 function DiscoveryStackScreen() {
-    return(
+    return (
         <DiscoveryStack.Navigator>
             <DiscoveryStack.Screen
                 name="DiscoveryScreen"
                 component={Discovery}
-                options={{headerShown: false}} />
+                options={{headerShown: false}}/>
             <DiscoveryStack.Screen
                 name="CarDetailsScreen"
                 component={CarDetailScreen}/>
@@ -25,13 +25,14 @@ function DiscoveryStackScreen() {
 }
 
 const MyRentalsStack = createNativeStackNavigator<NavigatorParamList>();
+
 function MyRentalsStackScreen() {
-    return(
+    return (
         <MyRentalsStack.Navigator>
             <MyRentalsStack.Screen
                 name="MyRentalsScreen"
                 component={MyRentals}
-                options={{headerShown: false}} />
+                options={{headerShown: false}}/>
         </MyRentalsStack.Navigator>
     );
 }
@@ -40,12 +41,12 @@ function MyRentalsStackScreen() {
 const ProfileStack = createNativeStackNavigator<NavigatorParamList>();
 
 function ProfileStackScreen() {
-    return(
+    return (
         <ProfileStack.Navigator>
             <ProfileStack.Screen
                 name="ProfileScreen"
                 component={ContextExample}
-                options={{headerShown: false}} />
+                options={{headerShown: false}}/>
         </ProfileStack.Navigator>
     );
 }
@@ -56,11 +57,11 @@ export default function App() {
     return (
         <ThemeContextProvider>
             <NavigationContainer>
-                <Tab.Navigator screenOptions={{ headerShown: false}}>
+                <Tab.Navigator screenOptions={{headerShown: false}}>
                     <Tab.Screen
                         name="DiscoveryScreenStack"
                         component={DiscoveryStackScreen}
-                        options={{title: 'Discover'}} />
+                        options={{title: 'Discover'}}/>
                     <Tab.Screen
                         name="MyRentalsScreenStack"
                         component={MyRentalsStackScreen}
