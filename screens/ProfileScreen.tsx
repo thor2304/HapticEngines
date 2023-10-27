cd688b273c52eb058fe6b7b55d7f9ae9119e0a81
Queztex <queztex@gmail.com>
2023-10-27 11:29:34 +0200

    Added functionality to get user information from backend

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,10 +1,9 @@
 import {getDefaultStyleSheet} from "../components/Stylesheet"
 import {StyleSheetI} from "../components/types/StyleSheetTypes";
 import {Text, View, StyleSheet} from "react-native";
-import {ThemeContext} from "../components/ThemeContext";
-import React, {useContext} from "react";
+import React, {useState} from "react";
 import {DiscoveryProps} from "./ScreenParams";
-import {darkTheme, lightTheme} from "../components/Themes";
+import backendHandler from "../components/BackendHandler";
 
 /**
  * This is the profile screen, which shows information about the user.
@@ -12,23 +11,36 @@ import {darkTheme, lightTheme} from "../components/Themes";
  * @param navigation The navigation object passed to this screen, used to navigate to other screens
  */
 export function ProfileScreen({route, navigation} : DiscoveryProps) {
-    const context = useContext(ThemeContext);
-    const theme = context.theme
-
-    function changeState() {
-        context.setTheme(context.theme === darkTheme ? lightTheme : darkTheme)
-    }
 
     // Stylesheet used is the interface from ColorPalette.tsx
-    const styles: StyleSheetI = getDefaultStyleSheet()
+    const styles: StyleSheetI = getDefaultStyleSheet();
+
+
+    const allUsers: Backend.UserCollection = [];
+    let [data, setData] = useState(allUsers);
+
+    backendHandler.getUsers()
+        .then(myData => {
+            setData(myData)
+            console.log(data)
+        }).catch((e) => {
+        console.log(e)
+        console.log("caught error")
+    });
 
     return (
         <View style={styles.container}>
             {/*<Image*/}
             {/*    style={cStyle.image}*/}
             {/*    source={require('add image path here')} />*/}
-            <Text style={cStyle.name}>Chad Payne</Text>
-            <Text style={cStyle.email}>chad@beauty.com</Text>
+
+            // View created to be able to add edit icon besides name in the future.
+            <View>
+                <Text style={cStyle.name}>{item.name}</Text>
+            </View>
+            <Text style={cStyle.info}>{item.email}</Text>
+            <Text style={cStyle.info}>{item.phoneNumber}</Text>
+            <Text style={cStyle.info}>{item.billingAddress}</Text>
         </View>
     );
 }
@@ -37,9 +49,9 @@ const cStyle = StyleSheet.create({
     name: {
         fontFamily: 'Inter',
         fontSize: 32,
-        fontWeight: '700',
+        fontWeight: '700'
     },
-    email: {
+    info: {
         fontFamily: 'Inter',
         fontSize: 26,
         fontWeight: '700',
@@ -47,6 +59,6 @@ const cStyle = StyleSheet.create({
     },
     image: {
         width: 128,
-        height: 128,
-    },
+        height: 128
+    }
 });
