3fbc3e3039a7781be7f66b64f0fbd5e0dc704869
Rasmus Christiansen <rachristi@outlook.dk>
2023-10-28 12:40:44 +0200

    Push notification for android. Hardcoded PushToken.

--- components/BackgroundTask.tsx
+++ components/BackgroundTask.tsx
@@ -3,69 +3,50 @@ import React, { useState, useEffect } from 'react';
 import { BackgroundFetchStatus } from 'expo-background-fetch';
 import * as TaskManager from 'expo-task-manager';
 import * as BackgroundFetch from 'expo-background-fetch';
-import { usePushNotifications } from '../usePushNotifications';
 
 const BACKGROUND_FETCH_TASK = 'background-fetch';
 
-
-// Note: This does NOT need to be in the global scope and CAN be used in your React components!
-// 1. Define the task by providing a name and the function that should be executed
-// Note: This needs to be called in the global scope (e.g outside of your React components)
 TaskManager.defineTask(BACKGROUND_FETCH_TASK, async () => {
   const now = Date.now();
 
-  console.log(`Got background fetch call at date: ${new Date(now).toISOString()}`);
-
-  // fetch('https://exp.host/--/api/v2/push/send', {
-  //   method: 'POST',
-  //   headers: {
-  //     Accept: 'application/json',
-  //     'Accept-encoding': 'gzip, deflate',
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify({
-  //     "to": `${usePushNotifications()}`,
-  //     "title":"Background Fetch test",
-  //     "body": "Background Fetch just ran",
-  //     "priority": "high"
-  //   }),
-  // }),
+  fetch('https://exp.host/--/api/v2/push/send', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Accept-encoding': 'gzip, deflate',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      "to": `ExponentPushToken[2ATCe1Is0q4YokiC0GZBr5]`,
+      "title": "Background Fetch test",
+      "body": "Background Fetch just ran",
+      "priority": "high"
+    }),
+  }),
 
     console.log(`Got background fetch call at date: ${new Date(now).toISOString()}`);
-    
-    // Be sure to return the successful result type!
-    return BackgroundFetch.BackgroundFetchResult.NewData;
-  });
+  return BackgroundFetch.BackgroundFetchResult.NewData;
+});
 
-// 2. Register the task at some point in your app by providing the same name,
-// and some configuration options for how the background fetch should behave
 async function registerBackgroundFetchAsync() {
   return BackgroundFetch.registerTaskAsync(BACKGROUND_FETCH_TASK, {
     minimumInterval:  0.1, // 15 minutes
-    stopOnTerminate: true, // android only
-    startOnBoot: true, // android only
+    stopOnTerminate: true,
+    startOnBoot: true, 
   });
 }
 
-// 3. (Optional) Unregister tasks by specifying the task name
-// This will cancel any future background fetch calls that match the given name
 async function unregisterBackgroundFetchAsync() {
   return BackgroundFetch.unregisterTaskAsync(BACKGROUND_FETCH_TASK);
 }
 
 export default function BackgroundFetchScreen(){
-  
-  const test  = usePushNotifications();
-  
-  console.log(test.expoPushToken, "background fetch screen")
-
   const [isRegistered, setIsRegistered] = React.useState(false);
   const [status, setStatus] = React.useState(null);
 
   React.useEffect(() => {
     checkStatusAsync();
   }, []);
-  
   
   const [fetchStatus, setFetchStatus] = useState<BackgroundFetchStatus | null>(null);
   
