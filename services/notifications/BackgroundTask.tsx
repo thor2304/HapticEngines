4e011b8a94d860c2dd25dd76fa9f7269f2ed7265
Rasmus Christiansen <rachristi@outlook.dk>
2023-10-29 20:15:13 +0100

    Auto stash before merge of "notification-backgroundtask" and "origin/notification-backgroundtask"

--- services/notifications/BackgroundTask.tsx
+++ services/notifications/BackgroundTask.tsx
@@ -1,6 +1,6 @@
-import {StyleSheet, Text, View, Button} from 'react-native';
-import React, {useState, useEffect} from 'react';
-import {BackgroundFetchStatus} from 'expo-background-fetch';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { BackgroundFetchStatus } from 'expo-background-fetch';
 import * as TaskManager from 'expo-task-manager';
 import * as BackgroundFetch from 'expo-background-fetch';
 import * as Notifications from 'expo-notifications';
